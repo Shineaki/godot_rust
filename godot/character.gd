8b23c6e1933f5884c62a4787f688ecdf6925ad8a
Shineaki <halmos.adam@pm.me>
2025-07-01 00:07:46 +0200

    init movement && attack

---
+++ godot/character.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+func _input(event):
+	if event.is_action_pressed("Attack"):
+		attack()
+
+func attack():
+	$Hands/RightHandPosition/AnimationPlayer.play("Attack")
+
+@export var speed = 100
+
+func get_input():
+	var input_direction = Input.get_vector("Move_Left", "Move_Right", "Move_Up", "Move_Down")
+	velocity = input_direction * speed
+
+func _process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	if $AnimatedSprite2D.flip_h == (mouse_pos.x < position.x):
+		pass
+	else:
+		$Hands.scale.x *= -1
+		$AnimatedSprite2D.flip_h = mouse_pos.x < position.x
+	
+		
+
+func _physics_process(delta):
+	#$Hands.look_at()
+	get_input()
+	move_and_slide()
